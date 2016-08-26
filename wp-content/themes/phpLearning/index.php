54a77ed223c1f8f6f1eddf6fcdc6cd8bfa639380
Samantha Sunders <samanthasunders@gmail.com>
2016-08-26 17:36:15 -0500

    Started Book

--- wp-content/themes/phpLearning/index.php
+++ wp-content/themes/phpLearning/index.php
@@ -1,10 +1,19 @@
-<!DOCTYPE html>
 <html>
-  <head>
-        <link type="text/css" rel="stylesheet" href="style.css"/>
-    <title>Summing Things Up</title>
-  </head>
-  <body>
-        <?php echo 'Samantha Sunders' ?>
-  </body>
+<head>
+	<title>Aliens Abducted Me - Report and Abduction</title>
+</head>
+
+<body>
+	<h2>Aliens Abducted Me - Report an Abduction</h2>
+
+	<?php
+
+	$when_it_happened = $_POST['whenithappened'];
+	$how_long = $_POST['howlong'];
+	$alien_description = $_POST['description'];
+
+
+	?>
+
+</body>
 </html>
