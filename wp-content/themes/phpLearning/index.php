8fd4d5ee98e0883a3b35497dca86fb596544cebe
Samantha Sunders <samanthasunders@gmail.com>
2016-08-26 17:40:01 -0500

    adding on to chapter one

--- wp-content/themes/phpLearning/index.php
+++ wp-content/themes/phpLearning/index.php
@@ -11,6 +11,15 @@
 	$when_it_happened = $_POST['whenithappened'];
 	$how_long = $_POST['howlong'];
 	$alien_description = $_POST['description'];
+	$fang_spotted = $_POST['fangspotted'];
+	$email = $_POST['email'];
+
+	echo "Thanks for submitting the form.<br />";
+	echo "You Were abducted " . $when_it_happened;
+	echo " and were gone for " . $how_long . "<br />";
+	echo "Describe them: " . $alien_description . "<br />";
+	echo "Was Fang there? " . $fang_spotted . "<br />";
+	echo "Your email address is " . $email;
 
 
 	?>
