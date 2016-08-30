f82e35524d30635ee3ff9ec17d21e0a24d04b39c
Samantha Sunders <samanthasunders@gmail.com>
2016-08-29 19:39:50 -0500

    started over with book info

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<html>
+<head>
+	<title>Aliens Abducted Me - Report and Abduction</title>
+</head>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+<body>
+	<h2>Aliens Abducted Me - Report an Abduction</h2>
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+	<?php
+
+	$when_it_happened = $_POST['whenithappened'];
+	$how_long = $_POST['howlong'];
+	$alien_description = $_POST['description'];
+	$fang_spotted = $_POST['fangspotted'];
+	$email = $_POST['email'];
+
+	echo "Thanks for submitting the form.<br />";
+	echo "You Were abducted " . $when_it_happened;
+	echo " and were gone for " . $how_long . "<br />";
+	echo "Describe them: " . $alien_description . "<br />";
+	echo "Was Fang there? " . $fang_spotted . "<br />";
+	echo "Your email address is " . $email;
+
+
+	?>
+
+</body>
+</html>
